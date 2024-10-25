0e7a2b499b55e5dbb52e45e2e368afe7f7a2dbdf
eputty123 <78649589+eputty123@users.noreply.github.com>
2024-10-24 23:47:02 -0400

    Update DXVK_config_mover.sh
    complete overhaul, without the force flag

--- DXVK_config_mover.sh
+++ DXVK_config_mover.sh
@@ -2,5 +2,5 @@
 cd "$HOME/.local/share/Steam/steamapps/common"
 for game in *; do
 	if [ ! -d "$game" ]; then continue; fi
-	ln -svf "$HOME/.config/dxvk.conf" "$game/dxvk.conf"
+	ln -sv "$HOME/.config/dxvk.conf" "$game/dxvk.conf"
 done
